92393c562d95648bae8e2a5fafecc68d5a02e9d5
Kevin Diego <kevindiegodasilvasousa@gmail.com>
2022-12-21 00:00:46 -0300

    feat: Trabalhando na funcionalidade js de Escrita de dados Write()

--- js/index.js
+++ js/index.js
@@ -1,4 +1,11 @@
-function Write() {console.log("escrita")}
+function Write() {
+    /*verificar algo sobre contexto para que o site recarregue sempre*/
+    const display = document.getElementsByClassName("informationDisplay");    
+    let controle = 0;
+    while(controle < display.length) {
+        display[controle].className = "informationDisplay--active";
+    }
+}
 
 function Crypt() {console.log("crypt")}
 
